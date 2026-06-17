8d607702d6fd5388789edac644a6c8ba53451320
Salva <salvadorsolealvarez@gmail.com>
2026-06-17 12:19:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE = 'multi-cache-v2';
 
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/game.js',
-    '/leaderboard.js',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
+    '.',
+    'index.html',
+    'style.css',
+    'game.js',
+    'leaderboard.js',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
